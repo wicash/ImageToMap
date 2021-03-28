d43c8f69c6b930f515e672c6bb69e666b161c980
wicash <gianluca.wicash@gmail.com>
2021-03-29 01:15:24 +0200

    Farben Hinzugefügt

--- ImageToMap/src/ArrayKonverter.java
+++ ImageToMap/src/ArrayKonverter.java
@@ -2,6 +2,9 @@ import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class ArrayKonverter {
 	BufferedImage image;
 	int height;
@@ -15,72 +18,93 @@ public class ArrayKonverter {
 
 	 {
 
-	     if (image == null || image.getWidth() == 0 || image.getHeight() == 0)
-	         return null;
+	      final byte[] pixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
+	      final int width = image.getWidth();
+	      final int height = image.getHeight();
+	      final boolean hasAlphaChannel = image.getAlphaRaster() != null;
+	      // Hier wird überprüft, wie das Pixels Array aussieht.%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		     System.out.println("Die Laenge des pixels Arrays ist:" + pixels.length);	   
+		     int i = 0;
+		     while(i < pixels.length){
+		    	 System.out.println("Wert des Arrays fuer die " + i + " Stelle: " + pixels[i]);
+		            i++;
+		        }
+		     //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-	     // This returns bytes of data starting from the top left of the bitmap
-	     // image and goes down.
-	     // Top to bottom. Left to right.
-	     final byte[] pixels = ((DataBufferByte) image.getRaster()
-	             .getDataBuffer()).getData();
-
-	     final int width = image.getWidth();
-	     final int height = image.getHeight();
-	     
-	   
-
-	     int[][] result = new int[height][width];
-
-	     boolean done = false;
-	     boolean alreadyWentToNextByte = false;
-	     int byteIndex = 0;
-	     int row = 0;
-	     int col = 0;
-	     int numBits = 0;
-	     byte currentByte = pixels[byteIndex];
-	     while (!done)
-	     {
-	         alreadyWentToNextByte = false;
-
-	         result[row][col] = (currentByte & 0x80) >> 7;
-	         currentByte = (byte) (((int) currentByte) << 1);
-	         numBits++;
-
-	         if ((row == height - 1) && (col == width - 1))
-	         {
-	             done = true;
+	      int[][] result = new int[height][width];
+	      if (hasAlphaChannel) {
+	         final int pixelLength = 4;
+	         for (int pixel = 0, row = 0, col = 0; pixel + 3 < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += (((int) pixels[pixel] & 0xff) << 24); // alpha
+	            argb += ((int) pixels[pixel + 1] & 0xff); // blue
+	            argb += (((int) pixels[pixel + 2] & 0xff) << 8); // green
+	            argb += (((int) pixels[pixel + 3] & 0xff) << 16); // red
+	            result[row][col] = argb;
+	            col++;
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	            }
 	         }
-	         else
-	         {
-	             col++;
-
-	             if (numBits == 8)
-	             {
-	                 currentByte = pixels[++byteIndex];
-	                 numBits = 0;
-	                 alreadyWentToNextByte = true;
-	             }
-
-	             if (col == width)
-	             {
-	                 row++;
-	                 col = 0;
-
-	                 if (!alreadyWentToNextByte)
-	                 {
-	                     currentByte = pixels[++byteIndex];
-	                     numBits = 0;
-	                 }
-	             }
+	      } else {
+	         final int pixelLength = 3;
+	         for (int pixel = 0, row = 0, col = 0; pixel + 2 < pixels.length; pixel += pixelLength) {
+	            int argb = 0;
+	            argb += 1; // 255 alpha
+	            argb += ((int) pixels[pixel]); // blue
+	            argb += (((int) pixels[pixel + 1])); // green
+	            argb += (((int) pixels[pixel + 2])); // red
+	            
+	            if (argb == -2) { //Hintergrund "weiß"
+	            	result[row][col] = 0;
+		            col++;
+	            } 
+	            else if (argb == 46) { //Farbe "Rot"
+	            	result[row][col] = 2;
+		            col++;
+	            }
+	            else if (argb == 5) { //Farbe "Schwarz"
+	            	result[row][col] = 1;
+		            col++;
+	            } 
+	            else if (argb == 84) { //Farbe "Blau"
+	            	result[row][col] = 3;
+		            col++;
+	            } 
+	            else if (argb == -14) { //Farbe "Gelb"
+	            	result[row][col] = 4;
+		            col++;
+	            } 
+	            else if (argb == 32) { //Farbe "Grün"
+	            	result[row][col] = 5;
+		            col++;
+	            } 
+	            else {
+	            	result[row][col] = argb;
+		            col++;
+		            System.out.println("Ubekannte Farbe");
+		            
+	            }
+	            
+	            
+	            if (col == width) {
+	               col = 0;
+	               row++;
+	               
+	            }
 	         }
-	     }
+	      }
 
-	     return result;
-	 }
+	      return result;
+	   }
+	 
 	 
 	 int[][] giveArray(){
 		 
 		 return this.convertToArray(image);
 		 
+		 
 	 }
+	 
 }
--- ImageToMap/src/ByteArrayConversion.java
+++ ImageToMap/src/ByteArrayConversion.java
@@ -4,7 +4,7 @@ import javax.imageio.*;
 
 public class ByteArrayConversion {
 	 public static void main(String[] args) throws Exception {
-	      BufferedImage image = ImageIO.read(new File("sample3.bmp")); //To-Do: Muss es eine monochromatische BMP sein?
+	      BufferedImage image = ImageIO.read(new File("test1.bmp")); //To-Do: Muss es eine monochromatische BMP sein?
 	      ByteArrayOutputStream output = new ByteArrayOutputStream();
 	      ImageIO.write(image, "jpg", output );
 	      byte [] data = output.toByteArray();
@@ -13,8 +13,7 @@ public class ByteArrayConversion {
 	      ArrayKonverter konverter = new ArrayKonverter(image);
 	      int[][] temp = konverter.giveArray();
 	      
-	      System.out.println(temp.length);
-	      System.out.println(temp[1].length);
+	      System.out.println("Die Auflösung der Map ist " + temp.length + "x" + temp[1].length + " Pixel.");
 	      for(int y=0; y<temp.length; y++) {
 	    	  System.out.println();
 	    	  for(int x=0; x<temp[y].length; x++) {
